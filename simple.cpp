13a06933408051054b71b3ca66f733fdc8a046ff
Buzz-coder <103954308+Buzz-coding-217@users.noreply.github.com>
2023-03-15 01:46:21 +0530

    Update simple.cpp

--- simple.cpp
+++ simple.cpp
@@ -2,46 +2,32 @@
 #include <iostream>
 using namespace std;
 #include <iomanip>
+#define N 100
 
 int main()
 {
-    int a[100][100], b[100][100], mult[100][100], r1 = 100, c1 = 100, r2 = 100, c2 = 100, i, j, k;
-
-    while (c1!=r2)
-    {
-        cout << "Error! column of first matrix not equal to row of second.";
-
-        cout << "Enter rows and columns for first matrix: ";
-        cin >> r1 >> c1;
-
-        cout << "Enter rows and columns for second matrix: ";
-        cin >> r2 >> c2;
-    }
+    // Initialising the variables
+    int a[N][N], b[N][N], mult[N][N], r1 = N, c1 = N, r2 = N, c2 = N, i, j, k;
     
+    // Initialsing the matrixs
     for(i = 0; i < r1; ++i)
         for(j = 0; j < c1; ++j)
         {
             a[i][j] = rand() % 20;
-        }
-    
-    for(i = 0; i < r2; ++i)
-        for(j = 0; j < c2; ++j)
-        {
             b[i][j] = rand() % 20;
-        }
-    for(i = 0; i < r1; ++i)
-        for(j = 0; j < c2; ++j)
-        {
             mult[i][j]=0;
         }
+    // Calculating the time
     auto start = std::chrono::high_resolution_clock::now();
-    
+
+    //Multiplying the matrix using the algorithm
     for(i = 0; i < r1; ++i)
         for(j = 0; j < c2; ++j)
             for(k = 0; k < c1; ++k)
             {
                 mult[i][j] += a[i][k] * b[k][j];
             }
+    
     auto end = std::chrono::high_resolution_clock::now(); // end time
     auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); // duration in nanoseconds
     std::cout << "Task duration: " << std::fixed << std::setprecision(9) << duration << " nanoseconds" << std::endl;
