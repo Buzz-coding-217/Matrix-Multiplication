3c06bb0eaf8466a60affcd23505114d0d934f172
Buzz-coder <103954308+Buzz-coding-217@users.noreply.github.com>
2023-03-14 22:33:21 +0530

    Update pthread_multiplication.cpp

--- pthread_multiplication.cpp
+++ pthread_multiplication.cpp
@@ -9,11 +9,10 @@
 using namespace std;
 #include <iomanip>
 
-#define NUM_THREADS 8
-#define MAX 1000000
+#define NUM_THREADS 100
 pthread_mutex_t mutx;
 
-int a[10][10], b[10][10], mult[10][10], r1, c1, r2, c2, i, j, k;
+int a[100][100], b[100][100], mult[100][100], r1 = 100, c1 = 100, r2 = 100, c2 = 100, i, j, k;
 
 void *mat_mul(void *rowt)
 {
@@ -35,16 +34,7 @@ int main()
     pthread_mutex_init(&mutx, NULL);
 
     cout<<" the number of cores on this machine = "<<cores<<endl;
-  
-    
 
-    cout << "Enter rows and columns for first matrix: ";
-    cin >> r1 >> c1;
-    cout << "Enter rows and columns for second matrix: ";
-    cin >> r2 >> c2;
-
-    // If column of first matrix in not equal to row of second matrix,
-    // ask the user to enter the size of matrix again.
     while (c1!=r2)
     {
         cout << "Error! column of first matrix not equal to row of second.";
@@ -55,64 +45,35 @@ int main()
         cout << "Enter rows and columns for second matrix: ";
         cin >> r2 >> c2;
     }
-
-    // Storing elements of first matrix.
     cout << endl << "Enter elements of matrix 1:" << endl;
     for(i = 0; i < r1; ++i)
         for(j = 0; j < c1; ++j)
         {
-            cout << "Enter element a" << i + 1 << j + 1 << " : ";
-            cin >> a[i][j];
+            a[i][j] = rand() % 20;
         }
-
-    // Storing elements of second matrix.
     cout << endl << "Enter elements of matrix 2:" << endl;
     for(i = 0; i < r2; ++i)
-        for(j = 0; j < c2; ++j)
         {
-            cout << "Enter element b" << i + 1 << j + 1 << " : ";
-            cin >> b[i][j];
+            b[i][j] = rand() % 20;
         }
 
-    // Initializing elements of matrix mult to 0.
     for(i = 0; i < r1; ++i)
         for(j = 0; j < c2; ++j)
         {
             mult[i][j]=0;
         }
-
-    // Multiplying matrix a and b and storing in array mult.
     auto start = std::chrono::high_resolution_clock::now();
 
     pthread_t threads[NUM_THREADS];
     for (int rowt = 0; rowt < r1; rowt++) {
         pthread_create(&threads[rowt], NULL, mat_mul, (void *)rowt);
     }
-
+auto end = std::chrono::high_resolution_clock::now(); // end time
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); // duration in nanoseconds
+    std::cout << "Task duration: " << std::fixed << std::setprecision(9) << duration << " nanoseconds" << std::endl;
    for(int rowt = 0; rowt < r1; rowt++) {
        pthread_join(threads[rowt], NULL);
    }
-  
-    // for(i = 0; i < r1; ++i)
-    //     for(j = 0; j < c2; ++j)
-    //         for(k = 0; k < c1; ++k)
-    //         {
-    //             mult[i][j] += a[i][k] * b[k][j];
-    //         }
-
-  
-    auto end = std::chrono::high_resolution_clock::now(); // end time
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); // duration in nanoseconds
-    std::cout << "Task duration: " << std::fixed << std::setprecision(9) << duration << " nanoseconds" << std::endl;
-    // Displaying the multiplication of two matrix.
-    cout << endl << "Output Matrix: " << endl;
-    for(i = 0; i < r1; ++i)
-    for(j = 0; j < c2; ++j)
-    {
-        cout << " " << mult[i][j];
-        if(j == c2-1)
-            cout << endl;
-    }
 
     return 0;
 }
